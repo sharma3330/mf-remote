06ead1e179007a912fc6247283bc977bd77a85c3
Ashish Sharma <sharma3330@gmail.com>
2025-07-05 18:39:52 +0530

    update url

--- rspack.config.ts
+++ rspack.config.ts
@@ -30,7 +30,7 @@ export default defineConfig({
     // You need to set a unique value that is not equal to other applications
     uniqueName: "mf_remote",
     // publicPath must be configured if using manifest
-    publicPath: "https://mf-remote.vercel.app/",
+    publicPath: "https://mf-remote-ten.vercel.app/",
   },
 
   experiments: {
